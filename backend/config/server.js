4eb9e33ae1aada1426a09f04452730db5a0846e5
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2022-03-03 20:51:22 +0100

    Deploy

--- backend/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '43f7ce955fba64bae5a5f3783c7e2fcc'),
-    },
-  },
-});
--- backend/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '036eb926-c47e-44b6-8173-5df222ee109a'
-};
